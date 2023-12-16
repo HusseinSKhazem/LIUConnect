d4de5f41d95e1e7e4a5d18d6867af0816c2a172c
Dark <hkhzm02@gmail.com>
2023-12-16 10:04:07 -0800

    landing Page

---
+++ LIUConnect/Controllers/landingController.cs
@@ -0,0 +1,87 @@
+﻿using LIUConnect.Core.Models;
+using LIUConnect.EF;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LIUConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class landingController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public landingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetLast4byMajor")]
+        public async Task<IActionResult> GetLast4(string Email)
+        {
+            var student = await _context.Students.Where(s=>s.User.Email==Email).FirstOrDefaultAsync();  
+            if (student == null) 
+            {
+                return NotFound("StudentNotFound");
+            }
+            var vacancy = await _context.Vacancies.Where(v=>v.MajorID == student.MajorID).Include(v => v.Major)
+                .Include(v => v.Recruiter)
+                .OrderByDescending(v => v.VacancyId) 
+                .Take(4)
+                .Select(v => new
+                {
+                    v.VacancyId,
+                    v.JobOffer,
+                    v.Description,
+                    v.Requirements,
+                    v.WorkingHours,
+                    v.Status,
+                    v.Responsibility,
+                    v.salary,
+                    v.experience,
+                    v.workLocation,
+                    v.Recruiter.CompanyName,
+                    majorName = v.Major.MajorName,
+                    RecruiterUsername = v.Recruiter.User.Username,
+                })
+                .ToListAsync();
+
+            if (vacancy.Count == 0)
+            {
+                return NotFound("No vacancies Found");
+            }
+
+            return Ok(vacancy);
+        }
+
+         [HttpGet("most-applied")]
+        public async Task<ActionResult<IEnumerable<Vacancy>>> GetMostAppliedVacancies()
+        {
+            var mostAppliedVacancies = await _context.Vacancies
+                .OrderByDescending(v => v.Applications.Count)
+                .Take(4)
+                 .Include(v => v.Major)
+                .Include(v => v.Recruiter)
+                .Select(v => new
+                {
+                    v.VacancyId,
+                    v.Status,
+                    v.Description,
+                    v.Requirements,
+                    v.WorkingHours,
+                    v.JobOffer,
+                    majorName = v.Major.MajorName,
+                    RecruiterUsername = v.Recruiter.User.Username,
+                    companyName = v.Recruiter.CompanyName
+
+                }).ToListAsync();
+
+            if (mostAppliedVacancies == null || mostAppliedVacancies.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(mostAppliedVacancies);
+        }
+    }
+}
--- LIUConnect/obj/Debug/net7.0/LIUConnect.AssemblyInfo.cs
+++ LIUConnect/obj/Debug/net7.0/LIUConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LIUConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b97f309c67fd0e3aa30ad32400008dbf67c031d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649c2bdaf61efc3914ece4d63e72d430397c421f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LIUConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LIUConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
